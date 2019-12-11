bdee17a56493921dab88e591c1a3ec5b10432598
DarkoGNU <zyzanskidarek@gmail.com>
2019-12-11 19:59:20 +0100

    Try to make multiplayer work

--- src/RunMode.jl
+++ src/RunMode.jl
@@ -1,12 +1,16 @@
 function runMode()
-    while true
-        b1 = getTile() .+ (0, -1, 0) #first block under the player
-        b2 = b1 .+ (0, -1, 0) #second block under the player
+    players = getPlayerIds()
 
+    while true
         sleep(0.05) #Don't overload the API & wait before removal
-           if getBlock(b1) == PiCraft.SAND && getBlock(b2) == PiCraft.TNT
-            setBlock(b1, PiCraft.AIR)
-            setBlock(b2, PiCraft.AIR)
+        for pl in players
+            b1 = getTile(pl) .+ (0, -1, 0) #first block under the player
+            b2 = b1 .+ (0, -1, 0) #second block under the player
+
+            if getBlock(b1) == PiCraft.SAND && getBlock(b2) == PiCraft.TNT
+                setBlock(b1, PiCraft.AIR)
+                setBlock(b2, PiCraft.AIR)
+            end
         end
     end
 end
